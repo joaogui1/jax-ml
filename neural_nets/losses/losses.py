ba6d3924380eb9f6f1381c6fa9feb314a2cd4fbb
joaogui1 <joaoguilhermearujo@gmail.com>
2019-08-12 09:49:51 -0300

    added new losses

--- neural_nets/losses/losses.py
+++ neural_nets/losses/losses.py
@@ -1,7 +1,9 @@
 import jax.numpy as np
 
-def mse(y, y_pred): return np.linalg.norm(y - y_pred)**2
-def crossentropy(y, y_pred): return -np.sum(y * np.log(y_pred + np.finfo(float).eps) + (1 - y) * np.log(1 - y_pred + np.finfo(float).eps))
+def mean_squared_error(y, y_pred): return np.linalg.norm(y - y_pred)**2
+def mean_absolute_error(y, y_pred): return np.linalg.norm(y - y_pred, 1)
+def hinge(y, y_pred): return np.mean(np.maximum(1. - y * y_pred, 0.), axis=-1)
+def crossentropy(y, y_pred): return -np.sum(y * np.log(y_pred + np.finfo(float).eps) + (1. - y) * np.log(1. - y_pred + np.finfo(float).eps))
 
 
 def create_loss(net, loss):
