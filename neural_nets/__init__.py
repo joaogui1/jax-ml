41e07662efd40d92e539b63a656eea367f89fb3e
joaogui1 <joaoguilhermearujo@gmail.com>
2019-08-15 22:06:16 -0300

    fastai + jax

--- neural_nets/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import activations
-from . import losses
--- neural_nets/losses/losses.py
+++ neural_nets/losses/losses.py
@@ -1,11 +1,9 @@
 import jax.numpy as np
-from jax import vmap
 
 def mean_squared_error(y, y_pred): return np.mean(np.linalg.norm(y - y_pred)**2, axis=-1)
 def mean_absolute_error(y, y_pred): return np.mean(np.linalg.norm(y - y_pred, 1), axis=-1)
 def hinge(y, y_pred): return np.mean(np.maximum(1. - y * y_pred, 0.), axis=-1)
 def crossentropy(y, y_pred): return -np.sum(y * np.log(y_pred + np.finfo(float).eps) + (1. - y) * np.log(1. - y_pred + np.finfo(float).eps))
-
 
 def create_loss(net, loss):
     def loss_fn(params, inputs, targets):
